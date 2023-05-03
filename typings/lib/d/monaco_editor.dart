ee30009ad331c813c1d459f85118f51bda31c059
Tim Maffett <timmaffett@gmail.com>
2023-05-02 20:23:50 -0700

    added first attempt at monaco-editor

---
+++ typings/lib/d/monaco_editor.dart
@@ -0,0 +1,11 @@
+library typings.monacoeditor;
+
+import 'package:typings/annotations.dart';
+
+@Typings.npm(
+    package: 'monaco-editor',
+    version: '0.38.0',
+    dirName: 'monaco_editor',
+    contextCheck: 'go',
+    uses: ['core'])
+export 'monaco_editor/monaco_editor.dart';
